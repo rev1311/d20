1643b7d61de455a73ef30d665500df1ec73a7558
M <unmykx@gmail.com>
2022-08-28 08:18:25 -0700

    added crit fail and nat 20 messaging to rolls

--- roll.js
+++ roll.js
@@ -2,13 +2,17 @@ const handleRoll = (numberOfDie, sidedDie) => {
     const $display = document.querySelector('.display')
     const $div = document.createElement('div')
 
-    $div.innerText = `d${sidedDie} - ${Math.floor(Math.random() * sidedDie) + 1}`
+    const rolledA = Math.floor(Math.random() * sidedDie) + 1
+    $div.innerText = `d${sidedDie} - ${rolledA}`
     $display.append($div)
 
     if (numberOfDie > 1) {
         numberOfDie--
         handleRoll(numberOfDie, sidedDie)
     }
+
+    sidedDie == 20 && rolledA == 1 && $div.append('CRITICAL FAILURE')
+    rolledA == 20 && $div.append('NAT 20!!')
 
 }
 
