8e891fdc24280594e533f1d3e87de79f62995d56
M <unmykx@gmail.com>
2022-09-04 09:04:29 -0700

    added handleRoll func

--- roll.js
+++ roll.js
@@ -1,19 +1,35 @@
-const handleRoll = (numberOfDie, sidedDie) => {
+// const handleRoll = (numberOfDie, sidedDie) => {
+//     const $display = document.querySelector('.display')
+//     const $div = document.createElement('div')
+
+//     const rolledA = Math.floor(Math.random() * sidedDie) + 1
+//     $div.innerText = `d${sidedDie} - ${rolledA}`
+//     $display.append($div)
+
+//     if (numberOfDie > 1) {
+//         numberOfDie--
+//         handleRoll(numberOfDie, sidedDie)
+//     }
+
+//     sidedDie == 20 && rolledA == 1 && $div.append('CRITICAL FAILURE')
+//     rolledA == 20 && $div.append('NAT 20!!')
+
+// }
+
+const handleRoll = () => {
     const $display = document.querySelector('.display')
-    const $div = document.createElement('div')
+    const $total = document.querySelector('.total')
+    const dice = $display.children
+    let total = 0
 
-    const rolledA = Math.floor(Math.random() * sidedDie) + 1
-    $div.innerText = `d${sidedDie} - ${rolledA}`
-    $display.append($div)
-
-    if (numberOfDie > 1) {
-        numberOfDie--
-        handleRoll(numberOfDie, sidedDie)
+    for ( let die of dice ) {
+        const dieVal = die.firstChild.dataset.value
+        const rolledA = Math.floor(Math.random() * dieVal) + 1
+        
+        die.append(rolledA)
+        total += rolledA
     }
-
-    sidedDie == 20 && rolledA == 1 && $div.append('CRITICAL FAILURE')
-    rolledA == 20 && $div.append('NAT 20!!')
-
+    $total.innerText = total
 }
 
 // clears dice imgs and counts
@@ -22,23 +38,22 @@ const clearRolls = () => {
     $display.innerText = ''
 }
 
-const setRolls = (numberOfDie, sidedDie) => {
-    clearRolls()
-    handleRoll(numberOfDie, sidedDie)
-}
-
 // creates larger, colored dice to be rolled
 const setDice = (id) => {
     const $display = document.querySelector('.display')
     const $div = document.createElement('div')
+    const $div2 = document.createElement('div')
     const $id = document.querySelector(`#${id}`)
     const $node = $id.cloneNode()
 
     $node.classList.remove('filter')
+    $div2.classList.add('rollVal')
+    $div2.setAttribute('background-color', 'black')
+    $div2.setAttribute('text-align', 'center')
 
     if($display.childElementCount >= 6) return
 
-    $div.append($node)
+    $div.append($node, $div2)
     $display.append($div)
     
 }
@@ -47,13 +62,16 @@ const setDice = (id) => {
 document.addEventListener('click', (e) => {
     e.preventDefault()
     let ele = e.target
+    
     ele.id.includes('dice') && setDice(ele.id)
+    ele.id.includes('roll') && handleRoll()
+
 })
 
 // sets background image to random on window load
 const bgGen = () => {
-    const selectBkg = Math.floor(Math.random() * 17) + 1
     const $main = document.querySelector('.main-container')
+    const selectBkg = Math.floor(Math.random() * 17) + 1
     const img = `url(assets/d20_bg${selectBkg}.png)`
 
     $main.setAttribute('style', `background-image: ${img}`)
